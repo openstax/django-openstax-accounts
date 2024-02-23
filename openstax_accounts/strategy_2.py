1d82d2a06bea2fe38fbbc44729f0e2c8fdccd5f5
Michael Harrison <volo@rice.edu>
2024-02-23 15:04:28 -0600

    code cleanup

--- openstax_accounts/strategy_2.py
+++ openstax_accounts/strategy_2.py
@@ -1,15 +1,22 @@
 import jwe
 import jwt
 
+
 class Strategy2:
-    def __init__(self, signature_public_key, encryption_private_key):
+    def __init__(self, signature_public_key, encryption_private_key, logging_enabled=False):
         self.signature_public_key = signature_public_key
         self.signature_algorithm = 'RS256'
         self.encryption_algorithm = 'dir'
         self.encryption_method = 'A256GCM'
         self.encryption_private_key = encryption_private_key
+        self.logging_enabled = logging_enabled
 
     def decrypt(self, cookie):
+
+        # Decoding is the reverse of what Accounts does to encode a cookie:
+        # Accounts first signs the payload w/ the signature private key, then
+        # it next symmetric encrypts that result w/ the encryption private key.
+
         if not cookie:
             return None
 
@@ -24,7 +31,11 @@ class Strategy2:
             )
 
             return Payload(decoded_payload)
-        except:
+        except Exception:
+            if self.logging_enabled:
+                import logging
+                logging.exception("Could not decrypt cookie")
+
             return None
 
 class Payload:
